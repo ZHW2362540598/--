89e7aaa141cb2713fe6f21c5e2670cfdce8ffc8f
ZHW2362540598 <2362540598@qq.com>
2020-12-07 20:06:02 +0800

    commit

--- 信号大作业/矩形窗函数.py
+++ 信号大作业/矩形窗函数.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
 
 def rect_wave(x,c,c0):     #起点为c0，宽度为c的矩形波
      if x>=(c+c0):
@@ -10,8 +12,9 @@ def rect_wave(x,c,c0):     #起点为c0，宽度为c的矩形波
           r=1
      return r
 
-x=np.linspace(-4,4,1000)
-y=np.array([rect_wave(t,2.0,-1.0) for t in x])
+t=np.linspace(-4,4,1000)
+y=np.array([rect_wave(t,2.0,-1.0) for t in t])
 plt.ylim(-0.2,1.2)
-plt.plot(x,y)
+plt.title(u'矩形窗函数')
+plt.plot(t,y)
 plt.show()
