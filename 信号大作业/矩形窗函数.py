b4384a56d808125bba810c3bf2a7e858c1408214
ZHW2362540598 <2362540598@qq.com>
2020-12-07 20:10:24 +0800

    commit

--- 信号大作业/矩形窗函数.py
+++ 信号大作业/矩形窗函数.py
@@ -13,7 +13,7 @@ def rect_wave(x,c,c0):     #起点为c0，宽度为c的矩形波
      return r
 
 t=np.linspace(-4,4,1000)
-y=np.array([rect_wave(t,2.0,-1.0) for t in t])
+y=np.array([rect_wave(t,2,-1) for t in t])
 plt.ylim(-0.2,1.2)
 plt.title(u'矩形窗函数')
 plt.plot(t,y)
